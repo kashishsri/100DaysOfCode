8fa56b16c0d1bde3d6abe9eb525caab3edc92617
kashishsri <kashishsrivastava869@gmail.com>
2024-07-06 12:55:59 +0530

    Created our Day 4 Project

--- Day4-RockPaperScissor.py
+++ Day4-RockPaperScissor.py
@@ -1,12 +1,3 @@
-# # learn about random module
-# import random
-
-# random_int = random.randint(1,10)
-# print(random_int)
-
-# random_float = random.random()
-# print(random_float)
-
 import random
 
 rock = '''
