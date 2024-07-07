ab90ec2e2f1ccf2cd2246bd8a2f8d7bc8d54003e
kashishsri <kashishsrivastava869@gmail.com>
2024-07-07 23:06:50 +0530

    Understanding Functions and Loops.

---
+++ Day6.py
@@ -0,0 +1,15 @@
+# Understanding FUNCTIONS
+# Defining function
+def my_function():
+    print("hello")
+    print("bye")
+
+# Calling function
+my_function()
+
+# Understanding WHILE LOOPS
+
+number = 10
+while(number > 0):
+    print(number)
+    number -= 1
