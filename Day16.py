14d1094c066011d0c1089b360645b503c00d3813
kashishsri <kashishsrivastava869@gmail.com>
2024-07-15 13:56:02 +0530

    Use of Turtle module

---
+++ Day16.py
@@ -0,0 +1,21 @@
+# Understanding OOP using Turtle
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("DarkMagenta")
+timmy.forward(100)
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
+
+
+
+
+
