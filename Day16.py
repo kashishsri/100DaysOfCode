37022d9369681b70dde9ff61324b9e4b08ad4d8d
kashishsri <kashishsrivastava869@gmail.com>
2024-07-15 14:35:58 +0530

    Coffee Machine Project using OOP

--- Day16.py
+++ Day16.py
@@ -1,15 +1,30 @@
-# Installing outside package and using it
+# Coffee Machine Project using OOP
 
-from prettytable import PrettyTable
-table = PrettyTable()
-table.align = "c"
-table.add_column("PokemonName", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-print(table)
+coffee_maker_obj = CoffeeMaker()
+menu_obj = Menu()
+money_machine_obj = MoneyMachine()
 
+coffee_machine_on = True
 
-
-
-
-
+while coffee_machine_on:
+    options = menu_obj.get_items()
+    choice = input(f"What would you like? ({options}) : ")
+    if choice == "off":
+        coffee_machine_mode = False
+    elif choice == "report":
+        # TODO 1: Print report
+        coffee_maker_obj.report()
+        money_machine_obj.report()
+    else:
+        drink = menu_obj.find_drink(choice)
+        # TODO 2: Check resources sufficient?
+        if coffee_maker_obj.is_resource_sufficient(drink):
+            # TODO 3: Process coins
+            # TODO 4: Check transaction successful?
+            if money_machine_obj.make_payment(drink.cost):
+                # TODO 5: Make coffee
+                coffee_maker_obj.make_coffee(drink)
