5448c37d8ad2506c7001309def12168d404ad8af
kashishsri <kashishsrivastava869@gmail.com>
2024-07-15 13:57:44 +0530

    Installing outside package

--- Day16.py
+++ Day16.py
@@ -1,18 +1,12 @@
-# Understanding OOP using Turtle
+# Installing outside package and using it
 
-from turtle import Turtle, Screen
+from prettytable import PrettyTable
+table = PrettyTable()
+table.align = "c"
+table.add_column("PokemonName", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("DarkMagenta")
-timmy.forward(100)
-
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
-
+print(table)
 
 
 
